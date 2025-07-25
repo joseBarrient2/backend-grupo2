2ebf4f5702f5c406ba4bce2503833f9ce806982c
Javier <Delgadosalcedojavier@gmail.com>
2025-07-24 21:44:27 -0500

    fix: revert CORS origin configuration to allow any origin

--- cors.js
+++ cors.js
@@ -2,8 +2,8 @@ const cors = require('cors');
 
 // Configuración de CORS - Solo para www.curacao.com
 const corsOptions = {
-  origin: ['https://enmat.pockethost.io'], 
-  //origin: true,  // Permitir cualquier origen
+  //origin: ['https://enmat.pockethost.io'], 
+  origin: true,  // Permitir cualquier origen
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
   credentials: true, // Permitir cookies y headers de autenticación
