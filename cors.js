da079b604beaa36d4cf521f2652ba00496e8a2cd
Javier <Delgadosalcedojavier@gmail.com>
2025-07-24 21:15:51 -0500

    sda

--- cors.js
+++ cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 // Configuración de CORS - Solo para www.curacao.com
 const corsOptions = {
-  origin: ['https://enmat.pockethost.io/'], 
+  origin: ['https://enmat.pockethost.io'], 
   //origin: true,  // Permitir cualquier origen
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
