b6674359073a935b523c33e4d493f3f0c2b912a6
joseBarrient2 <joseph00788@hotmail.com>
2025-08-03 19:37:46 -0500

    cambio conexion BD

--- conexiondb.js
+++ conexiondb.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'jota', 
-  /*host: 'dpg-d21dshbe5dus739qfq9g-a.oregon-postgres.render.com',*/
-  host: 'dpg-d241k41r0fns73am15ug-a',
+  host: 'dpg-d241k41r0fns73am15ug-a.oregon-postgres.render.com',
+  /*host: 'dpg-d241k41r0fns73am15ug-a',*/
   database: 'apijwt_exc2', 
   password: 'jX8gJkGCJ7dqPN1Ky4rApGTLWObJnJBr', 
   port: 5432,
