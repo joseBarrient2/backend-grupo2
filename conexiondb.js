f8e13257c7307ab58bda7fe70d009619bfe73196
joseBarrient2 <joseph00788@hotmail.com>
2025-09-21 20:08:38 -0500

    cambio de nombre de variables de entorno

--- conexiondb.js
+++ conexiondb.js
@@ -3,12 +3,12 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-  user: process.env.USER, 
+  user: process.env.DB_USER, 
   host: process.env.DB_HOST,
   /*host: 'dpg-d241k41r0fns73am15ug-a',*/
-  database: process.env.DATABASE, 
-  password: process.env.PASSWORD, 
-  port: process.env.PORT,
+  database: process.env.DB_DATABASE, 
+  password: process.env.DB_PASSWORD, 
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false
   }
